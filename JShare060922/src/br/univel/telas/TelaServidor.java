f43e519f8ff5a8b1ae8fdab9bed243fa3ec826e4
RHODEN <RHODEN@univel.br>
2016-04-18 12:45:48 -0300

    Jogados alguns itens para o escopo global para facilitar o trabalho com eles durante a programação

--- JShare060922/src/br/univel/telas/TelaServidor.java
+++ JShare060922/src/br/univel/telas/TelaServidor.java
@@ -6,15 +6,15 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.List;
-import java.util.Map;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
@@ -22,7 +22,6 @@ import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 import javax.swing.border.EmptyBorder;
 
-import br.univel.arquivos.Arquivo;
 import br.univel.comum.Cliente;
 import br.univel.comum.Servidor;
 
@@ -37,7 +36,9 @@ public class TelaServidor extends JFrame implements Servidor {
 	private int intPorta = 5050;
 	private JTextArea textArea;
 	private SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy H:mm:ss:SSS");
-
+	private Servidor servidor;
+	private Registry registry;
+	private JButton btnConectar;
 	/**
 	 * Launch the application.
 	 */
@@ -64,58 +65,58 @@ public class TelaServidor extends JFrame implements Servidor {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
 		JLabel lblTelaInicial = new JLabel("Servidor");
 		lblTelaInicial.setBounds(0, 0, 424, 26);
 		lblTelaInicial.setFont(new Font("Tahoma", Font.BOLD, 21));
 		lblTelaInicial.setHorizontalAlignment(SwingConstants.CENTER);
 		contentPane.add(lblTelaInicial);
-		
+
 		JLabel lblNewLabel = new JLabel("I P");
 		lblNewLabel.setBounds(15, 43, 46, 43);
 		contentPane.add(lblNewLabel);
-		
-		JButton btnConectar = new JButton("Conectar");
+
+		btnConectar = new JButton("Conectar");
 		btnConectar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				
+
 				iniciarServico();
-				
+
 			}
 		});
 		btnConectar.setBounds(316, 53, 89, 23);
 		contentPane.add(btnConectar);
-		
+
 		JButton btnPararOServio = new JButton("Parar o servi\u00E7o");
 		btnPararOServio.setBounds(415, 53, 109, 23);
 		contentPane.add(btnPararOServio);
-		
+
 		JLabel lblPorta = new JLabel("Porta");
 		lblPorta.setBounds(168, 57, 46, 14);
 		contentPane.add(lblPorta);
-		
+
 		textField = new JTextField();
 		textField.setText("127.0.0.1");
 		textField.setBounds(66, 54, 92, 20);
 		contentPane.add(textField);
 		textField.setColumns(10);
-		
+
 		textField_1 = new JTextField();
 		textField_1.setText("5050");
 		textField_1.setBounds(217, 54, 89, 20);
 		contentPane.add(textField_1);
 		textField_1.setColumns(10);
-		
+
 		JScrollPane scrollPane = new JScrollPane();
 		scrollPane.setBounds(15, 419, 544, -318);
 		contentPane.add(scrollPane);
-		
+
 		textArea = new JTextArea();
 		textArea.setEnabled(false);
 		textArea.setBounds(25, 87, 534, 338);
 		contentPane.add(textArea);
 	}
-	
+
 	private void mostrar(String string) {
 		textArea.append(sdf.format(new Date()));
 		textArea.append(" -> ");
@@ -124,74 +125,46 @@ public class TelaServidor extends JFrame implements Servidor {
 	}
 
 	protected void iniciarServico() {
-		
-		
-		
+
+
+
 		try{
-			servidor = (IServer)) UnicastRemoteObject.exportObject(this, 0);
+			servidor = (Servidor) UnicastRemoteObject.exportObject(this, 0);
 			registry = LocateRegistry.createRegistry(intPorta);
 			registry.rebind(Servidor.NOME, servidor);
-			
+
 			mostrar("Servico iniciado");
-			
-			btnConectar
+
+			btnConectar.setEnabled(false);
+		}catch (RemoteException e) {
+			JOptionPane.showMessageDialog(this, "Erro criando registro, verifique se a porta já não está sendo usada.");
+			e.printStackTrace();
 		}
-		
-		
-		
-	}
 
-	@Override
-	public void registrarCliente(Cliente c) throws RemoteException {
-		// TODO Auto-generated method stub
-		
-	}
 
-	@Override
-	public void publicarListaArquivos(Cliente c, List<Arquivo> lista) throws RemoteException {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public Map<Cliente, List<Arquivo>> procurarArquivo(String nome) throws RemoteException {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public byte[] baixarArquivo(Arquivo arq) throws RemoteException {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void desconectar(Cliente c) throws RemoteException {
-		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
 	public void entrarNoChat(String nome, Cliente cliente) throws RemoteException {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void enviarMensagem(String remetente, String destinatario, String mensagem) throws RemoteException {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void enviarMensagemPublica(String remetente, String mensagem) throws RemoteException {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void sair(String nome) throws RemoteException {
 		// TODO Auto-generated method stub
-		
+
 	}
 }
