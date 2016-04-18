fb3f4c61d20a165c57eb14754a0728613ea0cda5
RHODEN <RHODEN@univel.br>
2016-04-18 01:05:28 -0300

    Criação dos pacotes para melhor separação das informações, criação da tela do servidor

---
+++ JShare060922/src/br/univel/telas/TelaServidor.java
@@ -0,0 +1,50 @@
+package br.univel.telas;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import java.awt.Font;
+
+public class TelaServidor extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					TelaServidor frame = new TelaServidor();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public TelaServidor() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+		
+		JLabel lblTelaInicial = new JLabel("Servidor");
+		lblTelaInicial.setFont(new Font("Tahoma", Font.BOLD, 21));
+		lblTelaInicial.setHorizontalAlignment(SwingConstants.CENTER);
+		contentPane.add(lblTelaInicial, BorderLayout.NORTH);
+	}
+
+}
