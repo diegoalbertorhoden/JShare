eb3195ed683468d6151337e2724627d659601731
RHODEN <RHODEN@univel.br>
2016-05-05 00:15:01 -0300

    criação do método desconectar com try catch de Remote Exception

--- JShare060922/src/br/univel/telas/TelaCliente.java
+++ JShare060922/src/br/univel/telas/TelaCliente.java
@@ -8,28 +8,28 @@ import java.rmi.RemoteException;
 import java.util.List;
 import java.util.Map;
 
+import javax.swing.AbstractButton;
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
 import javax.swing.JTextField;
+import javax.swing.SwingConstants;
 
 import br.dagostini.jshare.comum.pojos.Arquivo;
 import br.dagostini.jshare.comun.Cliente;
 import br.dagostini.jshare.comun.IServer;
-import javax.swing.SwingConstants;
-import javax.swing.JComboBox;
-import javax.swing.JScrollBar;
-import javax.swing.JTable;
-import java.awt.GridBagLayout;
-import java.awt.GridBagConstraints;
-import java.awt.Insets;
-import javax.swing.JScrollPane;
 
 public class TelaCliente extends JFrame implements IServer{
 
 	private static final long serialVersionUID = -1981548995134350542L;
 	private Object registry;
 	private IServer servidor;
+	private JButton btnConectar;
+	private JButton btnDesconectar;
+	private Cliente cliente;
 
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
@@ -37,11 +37,45 @@ public class TelaCliente extends JFrame implements IServer{
 				try {
 					TelaCliente frame = new TelaCliente();
 					frame.setVisible(true);
+					frame.configurar();
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
 			}
 		});
+	}
+
+	public void configurar() {
+
+		btnConectar.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+				conectar();
+			}
+		});
+
+		btnDesconectar.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+				desconectar();
+			}
+		});
+		
+	}
+
+	protected void desconectar() {
+		try{		
+			if(servidor !=null){
+				servidor.desconectar(cliente);
+				servidor = null;
+			}
+			
+			btnDesconectar.setEnabled(false);
+			btnConectar.setEnabled(true);
+			registry = null;
+			servidor = null;
+		}catch(RemoteException e){
+			e.printStackTrace();
+		}
+		
 	}
 
 	private JTextField txtNome;
@@ -55,85 +89,85 @@ public class TelaCliente extends JFrame implements IServer{
 	public TelaCliente() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 640, 480);
-												getContentPane().setLayout(null);
-										
-												JLabel lblNome = new JLabel("Nome Cliente");
-												lblNome.setBounds(0, 0, 93, 29);
-												lblNome.setFont(new Font("Tahoma", Font.PLAIN, 14));
-												getContentPane().add(lblNome);
-								
-										txtNome = new JTextField();
-										txtNome.setBounds(117, 6, 223, 20);
-										getContentPane().add(txtNome);
-										txtNome.setColumns(10);
-						
-								JLabel lblPorta = new JLabel("Porta");
-								lblPorta.setBounds(350, 6, 93, 17);
-								lblPorta.setFont(new Font("Tahoma", Font.PLAIN, 14));
-								getContentPane().add(lblPorta);
-						
-								txtPorta = new JTextField();
-								txtPorta.setBounds(384, 6, 55, 20);
-								txtPorta.setHorizontalAlignment(SwingConstants.CENTER);
-								txtPorta.setText("5050");
-								getContentPane().add(txtPorta);
-								txtPorta.setColumns(10);
-						
-								JLabel lblIpDeConexo = new JLabel("Ip de conex\u00E3o");
-								lblIpDeConexo.setBounds(0, 34, 93, 23);
-								lblIpDeConexo.setFont(new Font("Tahoma", Font.PLAIN, 14));
-								getContentPane().add(lblIpDeConexo);
-				
-						txtIp = new JTextField();
-						txtIp.setBounds(117, 37, 88, 20);
-						txtIp.setHorizontalAlignment(SwingConstants.CENTER);
-						txtIp.setText("127.0.0.1");
-						getContentPane().add(txtIp);
-						txtIp.setColumns(10);
-				
-				JLabel lblIpLocal = new JLabel("Ip local");
-				lblIpLocal.setBounds(215, 34, 42, 23);
-				lblIpLocal.setFont(new Font("Tahoma", Font.PLAIN, 14));
-				getContentPane().add(lblIpLocal);
-				
-				JComboBox comboBox = new JComboBox();
-				comboBox.setBounds(267, 37, 151, 20);
-				getContentPane().add(comboBox);
-				
-						JButton btnConectar = new JButton("Conectar");
-						btnConectar.setBounds(423, 34, 104, 23);
-						btnConectar.addActionListener(new ActionListener() {
-							public void actionPerformed(ActionEvent e) {
+		getContentPane().setLayout(null);
+
+		JLabel lblNome = new JLabel("Nome Cliente");
+		lblNome.setBounds(0, 0, 93, 29);
+		lblNome.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		getContentPane().add(lblNome);
+
+		txtNome = new JTextField();
+		txtNome.setBounds(117, 6, 223, 20);
+		getContentPane().add(txtNome);
+		txtNome.setColumns(10);
+
+		JLabel lblPorta = new JLabel("Porta");
+		lblPorta.setBounds(350, 6, 93, 17);
+		lblPorta.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		getContentPane().add(lblPorta);
+
+		txtPorta = new JTextField();
+		txtPorta.setBounds(384, 6, 55, 20);
+		txtPorta.setHorizontalAlignment(SwingConstants.CENTER);
+		txtPorta.setText("5050");
+		getContentPane().add(txtPorta);
+		txtPorta.setColumns(10);
+
+		JLabel lblIpDeConexo = new JLabel("Ip de conex\u00E3o");
+		lblIpDeConexo.setBounds(0, 34, 93, 23);
+		lblIpDeConexo.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		getContentPane().add(lblIpDeConexo);
+
+		txtIp = new JTextField();
+		txtIp.setBounds(117, 37, 88, 20);
+		txtIp.setHorizontalAlignment(SwingConstants.CENTER);
+		txtIp.setText("127.0.0.1");
+		getContentPane().add(txtIp);
+		txtIp.setColumns(10);
+
+		JLabel lblIpLocal = new JLabel("Ip local");
+		lblIpLocal.setBounds(215, 34, 42, 23);
+		lblIpLocal.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		getContentPane().add(lblIpLocal);
+
+		JComboBox comboBox = new JComboBox();
+		comboBox.setBounds(267, 37, 151, 20);
+		getContentPane().add(comboBox);
+
+		JButton btnConectar = new JButton("Conectar");
+		btnConectar.setBounds(423, 34, 104, 23);
+		btnConectar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
 
 
-								conectar();
+				conectar();
 
-							}
-						});
-						getContentPane().add(btnConectar);
-		
-				JButton btnDesconectar = new JButton("Desconectar");
-				btnDesconectar.setBounds(532, 34, 93, 23);
-				getContentPane().add(btnDesconectar);
-		
+			}
+		});
+		getContentPane().add(btnConectar);
+
+		JButton btnDesconectar = new JButton("Desconectar");
+		btnDesconectar.setBounds(532, 34, 93, 23);
+		getContentPane().add(btnDesconectar);
+
 		JLabel lblArquivoProcurado = new JLabel("Arquivo Procurado");
 		lblArquivoProcurado.setBounds(0, 62, 182, 23);
 		lblArquivoProcurado.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		getContentPane().add(lblArquivoProcurado);
-		
+
 		txtProcurado = new JTextField();
 		txtProcurado.setBounds(187, 62, 291, 20);
 		getContentPane().add(txtProcurado);
 		txtProcurado.setColumns(10);
-		
+
 		JButton btnBuscar = new JButton("Buscar");
 		btnBuscar.setBounds(513, 62, 65, 23);
 		getContentPane().add(btnBuscar);
-		
+
 		JScrollPane scrollPane = new JScrollPane();
 		scrollPane.setBounds(0, 90, 620, 346);
 		getContentPane().add(scrollPane);
-		
+
 		tblArquivos = new JTable();
 		scrollPane.setViewportView(tblArquivos);
 	}
